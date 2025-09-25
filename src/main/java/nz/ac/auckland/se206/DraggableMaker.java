cd1df656e57400f2d186a1c8d94544ff7340c9e4
Vincent <vung610@aucklanduni.ac.nz>
2025-09-25 22:09:24 +1200

    added in some comments and updated cursor visuals in room

--- src/main/java/nz/ac/auckland/se206/DraggableMaker.java
+++ src/main/java/nz/ac/auckland/se206/DraggableMaker.java
@@ -8,6 +8,7 @@ public class DraggableMaker {
   private static double mouseY;
 
   public static void makeDraggable(Node node) {
+    // Record current mouse position
     node.setOnMousePressed(
         mouseEvent -> {
           mouseX = mouseEvent.getSceneX();
@@ -16,12 +17,14 @@ public class DraggableMaker {
 
     node.setOnMouseDragged(
         mouseEvent -> {
+          // Calculates new mouse position
           double deltaX = mouseEvent.getSceneX() - mouseX;
           double deltaY = mouseEvent.getSceneY() - mouseY;
 
           node.setLayoutX(node.getLayoutX() + deltaX);
           node.setLayoutY(node.getLayoutY() + deltaY);
 
+          // Update new mouse position
           mouseX = mouseEvent.getSceneX();
           mouseY = mouseEvent.getSceneY();
         });
